44ea71e3153a72be1024506509c71fce03bce60b
Edelways <113142949+jeffpharaon@users.noreply.github.com>
2025-02-18 09:57:20 +0300

    Update database.sql

--- DataBase/database.sql
+++ DataBase/database.sql
@@ -33,12 +33,12 @@ BEGIN
     CREATE TABLE [User]
     (
         UserID INT PRIMARY KEY IDENTITY,
-        UserSurname NVARCHAR(100) NOT NULL,
-        UserName NVARCHAR(100) NOT NULL,
-        UserPatronymic NVARCHAR(100) NOT NULL,
-        UserLogin NVARCHAR(MAX) NOT NULL,
-        UserPassword NVARCHAR(MAX) NOT NULL,
-        UserRole INT NOT NULL FOREIGN KEY REFERENCES [Role](RoleID)
+        UserSurname      NVARCHAR(100) NOT NULL,
+        UserName         NVARCHAR(100) NOT NULL,
+        UserPatronymic   NVARCHAR(100) NOT NULL,
+        UserLogin        NVARCHAR(MAX) NOT NULL,
+        UserPassword     NVARCHAR(MAX) NOT NULL,
+        UserRole         INT NOT NULL FOREIGN KEY REFERENCES [Role](RoleID)
     );
 END
 GO
@@ -60,15 +60,15 @@ BEGIN
     CREATE TABLE [Product]
     (
         ProductArticleNumber NVARCHAR(100) PRIMARY KEY,
-        ProductName NVARCHAR(MAX) NOT NULL,
-        ProductDescription NVARCHAR(MAX) NOT NULL,
-        ProductCategory NVARCHAR(MAX) NOT NULL,
-        ProductPhoto IMAGE NOT NULL,
-        ProductManufacturer NVARCHAR(MAX) NOT NULL,
-        ProductCost DECIMAL(19,4) NOT NULL,
+        ProductName          NVARCHAR(MAX) NOT NULL,
+        ProductDescription   NVARCHAR(MAX) NOT NULL,
+        ProductCategory      NVARCHAR(MAX) NOT NULL,
+        ProductPhoto         IMAGE NULL,  
+        ProductManufacturer  NVARCHAR(MAX) NOT NULL,
+        ProductCost          DECIMAL(19,4) NOT NULL,
         ProductDiscountAmount TINYINT NULL,
         ProductQuantityInStock INT NOT NULL,
-        ProductStatus NVARCHAR(MAX) NOT NULL
+        ProductStatus        NVARCHAR(MAX) NOT NULL
     );
 END
 GO
@@ -111,21 +111,28 @@ INSERT INTO [User] (
     UserPassword,
     UserRole
 )
-SELECT
-    [UserSurname],
-    [UserName],
-    [UserPatronymic],
-    [UserLogin],
-    [UserPassword],
-    [UserRole]
-FROM OPENROWSET(
-    'Microsoft.ACE.OLEDB.12.0',
-    'Excel 12.0 Xml;HDR=YES;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\user_import.xlsx',
-    'SELECT [UserSurname],[UserName],[UserPatronymic],[UserLogin],[UserPassword],[UserRole] FROM [Sheet1$]'
-) AS t;
-GO
-
-ALTER TABLE [Product] ALTER COLUMN ProductPhoto IMAGE NULL;
+SELECT 
+    x.UserSurname,
+    x.UserName,
+    x.UserPatronymic,
+    x.UserLogin,
+    x.UserPassword,
+    x.UserRole
+FROM
+(
+    SELECT 
+        CAST([UserSurname] AS NVARCHAR(100))     AS [UserSurname],
+        CAST([UserName] AS NVARCHAR(100))        AS [UserName],
+        CAST([UserPatronymic] AS NVARCHAR(100))  AS [UserPatronymic],
+        CAST([UserLogin] AS NVARCHAR(MAX))       AS [UserLogin],
+        CAST([UserPassword] AS NVARCHAR(MAX))    AS [UserPassword],
+        CAST([UserRole] AS INT)                  AS [UserRole]
+    FROM OPENROWSET(
+        'Microsoft.ACE.OLEDB.12.0',
+        'Excel 12.0 Xml;HDR=YES;IMEX=1;TypeGuessRows=0;ImportMixedTypes=Text;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\users_import.xlsx',
+        'SELECT [UserSurname],[UserName],[UserPatronymic],[UserLogin],[UserPassword],[UserRole] FROM [Table Data$]'
+    )
+) AS x;
 GO
 
 INSERT INTO [Product] (
@@ -141,21 +148,34 @@ INSERT INTO [Product] (
     ProductStatus
 )
 SELECT
-    [ProductArticleNumber],
-    [ProductName],
-    [ProductDescription],
-    [ProductCategory],
-    NULL, 
-    [ProductManufacturer],
-    [ProductCost],
-    [ProductDiscountAmount],
-    [ProductQuantityInStock],
-    [ProductStatus]
-FROM OPENROWSET(
-    'Microsoft.ACE.OLEDB.12.0',
-    'Excel 12.0 Xml;HDR=YES;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\products_import.xlsx',
-    'SELECT [ProductArticleNumber],[ProductName],[ProductDescription],[ProductCategory],[ProductManufacturer],[ProductCost],[ProductDiscountAmount],[ProductQuantityInStock],[ProductStatus] FROM [Sheet1$]'
-) AS t;
+    x.ProductArticleNumber,
+    x.ProductName,
+    x.ProductDescription,
+    x.ProductCategory,
+    NULL,  
+    x.ProductManufacturer,
+    x.ProductCost,
+    x.ProductDiscountAmount,
+    x.ProductQuantityInStock,
+    x.ProductStatus
+FROM
+(
+    SELECT 
+        CAST([ProductArticleNumber] AS NVARCHAR(100))       AS [ProductArticleNumber],
+        CAST([ProductName] AS NVARCHAR(MAX))                AS [ProductName],
+        CAST([ProductDescription] AS NVARCHAR(MAX))         AS [ProductDescription],
+        CAST([ProductCategory] AS NVARCHAR(MAX))            AS [ProductCategory],
+        CAST([ProductManufacturer] AS NVARCHAR(MAX))        AS [ProductManufacturer],
+        CAST([ProductCost] AS DECIMAL(19,4))                AS [ProductCost],
+        CAST([ProductDiscountAmount] AS TINYINT)            AS [ProductDiscountAmount],
+        CAST([ProductQuantityInStock] AS INT)               AS [ProductQuantityInStock],
+        CAST([ProductStatus] AS NVARCHAR(MAX))              AS [ProductStatus]
+    FROM OPENROWSET(
+        'Microsoft.ACE.OLEDB.12.0',
+        'Excel 12.0 Xml;HDR=YES;IMEX=1;TypeGuessRows=0;ImportMixedTypes=Text;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\products_import.xlsx',
+        'SELECT [ProductArticleNumber],[ProductName],[ProductDescription],[ProductCategory],[ProductManufacturer],[ProductCost],[ProductDiscountAmount],[ProductQuantityInStock],[ProductStatus] FROM [Лист1$]'
+    )
+) AS x;
 GO
 
 INSERT INTO [Order] (
@@ -164,14 +184,21 @@ INSERT INTO [Order] (
     OrderPickupPoint
 )
 SELECT
-    [OrderStatus],
-    [OrderDeliveryDate],
-    [OrderPickupPoint]
-FROM OPENROWSET(
-    'Microsoft.ACE.OLEDB.12.0',
-    'Excel 12.0 Xml;HDR=YES;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\order_import.xlsx',
-    'SELECT [OrderStatus],[OrderDeliveryDate],[OrderPickupPoint] FROM [Sheet1$]'
-) AS t;
+    x.OrderStatus,
+    x.OrderDeliveryDate,
+    x.OrderPickupPoint
+FROM
+(
+    SELECT 
+        CAST([OrderStatus] AS NVARCHAR(MAX))  AS [OrderStatus],
+        CAST([OrderDeliveryDate] AS DATETIME) AS [OrderDeliveryDate],
+        CAST([OrderPickupPoint] AS NVARCHAR(MAX)) AS [OrderPickupPoint]
+    FROM OPENROWSET(
+        'Microsoft.ACE.OLEDB.12.0',
+        'Excel 12.0 Xml;HDR=YES;IMEX=1;TypeGuessRows=0;ImportMixedTypes=Text;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\order_import.xlsx',
+        'SELECT [OrderStatus],[OrderDeliveryDate],[OrderPickupPoint] FROM [Table Data$]'
+    )
+) AS x;
 GO
 
 INSERT INTO [Pickups] (
@@ -180,26 +207,19 @@ INSERT INTO [Pickups] (
     Address
 )
 SELECT
-    [OrderID],
-    [PickupDate],
-    [Address]
-FROM OPENROWSET(
-    'Microsoft.ACE.OLEDB.12.0',
-    'Excel 12.0 Xml;HDR=YES;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\pickups_import.xlsx',
-    'SELECT [OrderID],[PickupDate],[Address] FROM [Sheet1$]'
-) AS t;
-GO
-
-INSERT INTO [OrderProduct] (
-    OrderID,
-    ProductArticleNumber
-)
-SELECT
-    [OrderID],
-    [ProductArticleNumber]
-FROM OPENROWSET(
-    'Microsoft.ACE.OLEDB.12.0',
-    'Excel 12.0 Xml;HDR=YES;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\order_products_import.xlsx',
-    'SELECT [OrderID],[ProductArticleNumber] FROM [Sheet1$]'
-) AS t;
+    x.OrderID,
+    x.PickupDate,
+    x.Address
+FROM
+(
+    SELECT
+        CAST([OrderID] AS INT)         AS [OrderID],
+        CAST([PickupDate] AS DATETIME) AS [PickupDate],
+        CAST([Address] AS NVARCHAR(255)) AS [Address]
+    FROM OPENROWSET(
+        'Microsoft.ACE.OLEDB.12.0',
+        'Excel 12.0 Xml;HDR=YES;IMEX=1;TypeGuessRows=0;ImportMixedTypes=Text;Database=D:\VsCodeProjects\.Curs\Edelweiss\задание\задание\resources\pickups_import.xlsx',
+        'SELECT [OrderID],[PickupDate],[Address] FROM [Sheet1$]'
+    )
+) AS x;
 GO
